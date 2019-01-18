8d6b00ad74d630669f0d30f512fe3324eb7d48ab
Adam Butterworth <abutterworth@edx.org>
2019-01-18 15:02:58 -0500

    Add banner and header atoms

--- src/components/UserAccount/index.jsx
+++ src/components/UserAccount/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Hyperlink } from '@edx/paragon';
+import { Icon, Hyperlink } from '@edx/paragon';
 import {
   Container, Row, Col,
+  Button,
+  FormGroup, Input, Label,
   Card, CardImg, CardBody, CardTitle,
 } from 'reactstrap';
 
-const certificates = [{ title: 'Certificate 1' }, { title: 'Certificate 2' }, { title: 'Certificate 3' }];
-const courses = [{ title: 'Course ' }, { title: 'Course 2' }, { title: 'Course 3' }];
 
-function renderEducationAndSocial() {
-  return (
+function UserAccount() {
+  const certificates = [{ title: 'Certificate 1' }, { title: 'Certificate 2' }, { title: 'Certificate 3' }];
+  const courses = [{ title: 'Course ' }, { title: 'Course 2' }, { title: 'Course 3' }];
+
+  const renderEducationAndSocial = () => (
     <React.Fragment>
       <h3>Education</h3>
       <p>Bachelor’s Degree</p>
@@ -45,78 +48,102 @@ function renderEducationAndSocial() {
       </dl>
     </React.Fragment>
   );
-}
 
-function UserAccount() {
+
   return (
-    <Container fluid>
-      <Row className="d-none d-md-block mb-3">
-        <Col md={4}>
-          <img className="mw-100 rounded-circle d-block" src="https://placeholdit.imgix.net/~text?txt=Avatar&w=120&h=120" alt="avatar" />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={4} lg={3}>
-          <Row className="mb-3">
-            <Col className="d-flex align-items-center d-md-block">
-              <div className="mr-3 d-md-none" style={{ flexBasis: '30%' }}>
-                <img className="mw-100 rounded-circle d-block" src="https://placeholdit.imgix.net/~text?txt=Avatar&w=200&h=200" alt="avatar" />
-              </div>
-              <div>
-                <h1 className="h2">Hermione Granger</h1>
-                <ul className="list-unstyled mb-0">
-                  <li>itslevioooosa20</li>
-                  <li>Member since 2017</li>
-                  <li>London, UK</li>
-                </ul>
-              </div>
-            </Col>
-          </Row>
-          <Row className="mb-3 d-none d-md-block">
-            <Col>
+    <div>
+      <div
+        style={{
+          backgroundImage: 'url(https://source.unsplash.com/featured/1000x200/?colored,pattern)',
+        }}
+        className="bg-banner d-none d-md-block p-relative"
+      >
+        <Container fluid>
+          <img className="avatar mw-100 p-absolute rounded-circle d-block" src="https://source.unsplash.com/featured/200x200/?face" alt="avatar" />
+        </Container>
+      </div>
+
+      <Container fluid className="pt-3">
+        <Row>
+          <Col md={4} lg={3}>
+            <Row className="mb-3">
+              <Col className="d-flex align-items-center d-md-block">
+                <div className="mr-3 d-md-none" style={{ flexBasis: '30%' }}>
+                  <img className="mw-100 rounded-circle d-block" src="https://source.unsplash.com/featured/200x200/?face" alt="avatar" />
+                </div>
+                <div>
+                  <h1 className="h2 mb-0">Hermione Granger</h1>
+                  <ul className="list-unstyled mb-0">
+                    <li className="mb-2">itslevioooosa20</li>
+                    <li>London, UK</li>
+                  </ul>
+                </div>
+              </Col>
+            </Row>
+            <Row className="mb-3 d-none d-md-block">
+              <Col>
+                {renderEducationAndSocial()}
+              </Col>
+            </Row>
+          </Col>
+          <Col md={8} lg={9} className="pt-md-1">
+
+            <h3>About Hermione</h3>
+            <p>These are some words about me and who I am as a person.</p>
+
+            <div className="d-md-none">
               {renderEducationAndSocial()}
-            </Col>
-          </Row>
-        </Col>
-        <Col md={8} lg={9}>
+            </div>
 
-          <h3>About Hermione</h3>
-          <p>These are some words about me and who I am as a person.</p>
+            <h3>My Certificates</h3>
+            <Row>
+              {certificates.map(({ title }) => (
+                <Col sm={6} lg={4}>
+                  <Card className="mb-4">
+                    <CardImg top src="https://placeholdit.imgix.net/~text?txt=Certificate&w=300&h=100" />
+                    <CardBody>
+                      <CardTitle>{title}</CardTitle>
+                    </CardBody>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
 
-          <div className="d-md-none">
-            {renderEducationAndSocial()}
-          </div>
+            <h3>Courses I’ve Taken</h3>
+            <Row>
+              {courses.map(({ title }) => (
+                <Col sm={6} lg={4}>
+                  <Card className="mb-4">
+                    <CardImg top src="https://placeholdit.imgix.net/~text?txt=Certificate&w=300&h=100" />
+                    <CardBody>
+                      <CardTitle>{title}</CardTitle>
+                    </CardBody>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
 
-          <h3>My Certificates</h3>
-          <Row>
-            {certificates.map(({ title }) => (
-              <Col sm={6} lg={4}>
-                <Card className="mb-4">
-                  <CardImg top src="https://placeholdit.imgix.net/~text?txt=Certificate&w=300&h=100" />
-                  <CardBody>
-                    <CardTitle>{title}</CardTitle>
-                  </CardBody>
-                </Card>
-              </Col>
-            ))}
-          </Row>
+        <h1>Utils for later</h1>
 
-          <h3>Courses I’ve Taken</h3>
-          <Row>
-            {courses.map(({ title }) => (
-              <Col sm={6} lg={4}>
-                <Card className="mb-4">
-                  <CardImg top src="https://placeholdit.imgix.net/~text?txt=Certificate&w=300&h=100" />
-                  <CardBody>
-                    <CardTitle>{title}</CardTitle>
-                  </CardBody>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+        <FormGroup>
+          <Label className="d-inline-block mr-2" size="sm" for="exampleSelect">Who can see this:</Label>
+          <Input className="d-inline-block w-auto" size="sm" type="select" name="select" id="exampleSelect">
+            <option>Just me</option>
+            <option>Everyone</option>
+          </Input>
+        </FormGroup>
+        <div>
+          <span className="small"><Icon className="fa fa-eye-slash" /> Everyone</span>
+          <span className="small"><Icon className="fa fa-eye" /> Just me</span>
+        </div>
+        <div>
+          <Button color="link">Discard Changes</Button>
+          <Button className="ml-2" color="primary">Save Changes</Button>
+        </div>
+      </Container>
+    </div>
   );
 }
 
