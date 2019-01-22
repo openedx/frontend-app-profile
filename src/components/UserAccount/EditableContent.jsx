a75dcc38eeddb6dc5ce10166a1769d6872fdfc83
Adam Butterworth <abutterworth@edx.org>
2019-01-22 15:35:23 -0500

    Add height animation to editable content

--- src/components/UserAccount/EditableContent.jsx
+++ src/components/UserAccount/EditableContent.jsx
@@ -1,24 +1,92 @@
+import React from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition } from 'react-transition-group';
 
+class EditableContent extends React.Component {
+  constructor(props) {
+    super(props);
 
-function EditableContent(props) {
-  const {
-    isEditing,
-    disabled,
-    renderStatic,
-    renderEditable,
-    renderEditing,
-  } = props;
+    this.container = React.createRef();
+    this.content = React.createRef();
 
-  if (disabled) {
-    return renderStatic(props);
+    this.state = {
+      height: 'auto',
+      containerRef: this.container,
+
+      // Mirroring props in state as described here:
+      // https://github.com/reactjs/rfcs/blob/master/text/0006-static-lifecycle-methods.md#state-derived-from-propsstate
+      mirroredIsEditing: props.isEditing,
+    };
+
+    this.onTransitionEnd = this.onTransitionEnd.bind(this);
   }
-  if (isEditing) {
-    return renderEditing(props);
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.mirroredIsEditing !== nextProps.isEditing) {
+      return {
+        height: prevState.containerRef.current.offsetHeight,
+        mirroredIsEditing: nextProps.isEditing,
+      };
+    }
+
+    return null;
   }
 
-  return renderEditable(props);
+  componentDidUpdate(prevProps) {
+    if (this.props.isEditing !== prevProps.isEditing) {
+      // See https://github.com/airbnb/javascript/issues/1875 for why ignoring this line in linter.
+      this.setState({ // eslint-disable-line react/no-did-update-set-state
+        height: this.content.current.offsetHeight,
+      });
+
+      const focusableElements = this.content.current.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+      if (focusableElements[0]) focusableElements[0].focus();
+    }
+  }
+
+  onTransitionEnd() {
+    this.setState({ height: 'auto' });
+  }
+
+  render() {
+    const {
+      isEditing,
+      disabled,
+      renderStatic,
+      renderEditable,
+      renderEditing,
+    } = this.props;
+
+    const transitionProps = {
+      timeout: 250,
+      classNames: 'crossfade',
+      unmountOnExit: true,
+      mountOnEnter: true,
+      onExited: this.onTransitionEnd,
+    };
+
+    return (
+      <div
+        ref={this.container}
+        className="editable-content-container"
+        style={{ height: this.state.height }}
+      >
+        <CSSTransition in={!isEditing} {...transitionProps}>
+          <div ref={!isEditing ? this.content : null} className="editable-content">
+            {disabled ? renderStatic(this.props) : renderEditable(this.props) }
+          </div>
+        </CSSTransition>
+
+        <CSSTransition in={isEditing} {...transitionProps}>
+          <div ref={isEditing ? this.content : null} className="editable-content">
+            {renderEditing(this.props)}
+          </div>
+        </CSSTransition>
+      </div>
+    );
+  }
 }
+
 
 EditableContent.propTypes = {
   isEditing: PropTypes.bool,
