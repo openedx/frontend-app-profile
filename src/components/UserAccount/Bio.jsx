f4af039ef5db2879f8d06093dacfa2ac46f393a8
Adam Butterworth <adamjbutterworth@gmail.com>
2019-02-08 17:16:37 -0500

    Move crossfade transition of in-context editable elements to a discrete component

--- src/components/UserAccount/Bio.jsx
+++ src/components/UserAccount/Bio.jsx
@@ -2,66 +2,130 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Input } from 'reactstrap';
 
-import EditableContent from './EditableContent';
 import EditButton from './EditButton';
 import Visibility from './Visibility';
+import TransitionReplace from './TransitionReplace';
+import EditControls from './EditControls';
 
 
-function Bio({
-  bio,
-  visibility,
-  title,
-  ...editableContentProps
-}) {
-  return (
-    <EditableContent
-      {...editableContentProps}
-      values={{
-        bio,
-        visibility,
-      }}
-      renderStatic={() => (
-        <React.Fragment>
-          <h3>{title}</h3>
-          <p>{bio}</p>
-        </React.Fragment>
-      )}
-      renderEditable={onClickEdit => (
-        <React.Fragment>
-          <h3>{title} <EditButton onClick={onClickEdit} /> <br /><Visibility to="Everyone" /></h3>
-          <p>{bio}</p>
-        </React.Fragment>
-      )}
-      renderEditing={(state, setState) => ( // eslint-disable-line no-unused-vars
-        <React.Fragment>
-          <h3>{title}</h3>
-          <Input
-            defaultValue={state.bio}
-            type="textarea"
-            name="text"
-            id="exampleText"
-            onChange={(e) => {
-              setState({
+class Bio extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      values: {
+        bio: props.bio,
+        visibility: props.visibility,
+      },
+    };
+
+    this.renderModes = {
+      editing: this.renderEditing.bind(this),
+      editable: this.renderEditable.bind(this),
+      static: this.renderStatic.bind(this),
+    };
+
+    this.onClickEdit = this.onClickEdit.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+    this.onClickCancel = this.onClickCancel.bind(this);
+  }
+
+  onClickEdit() {
+    this.props.onEdit(this.props.name);
+  }
+
+  onClickSave() {
+    this.props.onSave(this.props.name, this.state.values);
+  }
+
+  onClickCancel() {
+    this.props.onCancel(this.props.name);
+  }
+
+  renderStatic() {
+    return (
+      <React.Fragment key="static">
+        <h3>{this.props.title}</h3>
+        <p>{this.props.bio}</p>
+      </React.Fragment>
+    );
+  }
+
+  renderEditable() {
+    return (
+      <React.Fragment key="editable">
+        <h3>
+          {this.props.title}
+          <EditButton onClick={this.onClickEdit} /> <br />
+          <Visibility to={this.props.visibility} />
+        </h3>
+        <p>{this.props.bio}</p>
+      </React.Fragment>
+    );
+  }
+
+  renderEditing() {
+    return (
+      <React.Fragment key="editing">
+        <h3>{this.props.title}</h3>
+        <Input
+          defaultValue={this.props.bio}
+          type="textarea"
+          name="text"
+          id="exampleText"
+          onChange={(e) => {
+            this.setState({
+              values: {
                 bio: e.target.value,
-              });
-            }}
-          />
-        </React.Fragment>
-      )}
-    />
-  );
+                visibility: this.state.values.visibility,
+              },
+            });
+          }}
+        />
+        <EditControls
+          onCancel={this.onClickCancel}
+          onSave={this.onClickSave}
+          visibility={this.state.values.visibility}
+          onVisibilityChange={(e) => {
+            this.setState({
+              values: {
+                bio: this.state.values.bio,
+                visibility: e.target.value,
+              },
+            });
+          }}
+        />
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <TransitionReplace>
+        {this.renderModes[this.props.mode]()}
+      </TransitionReplace>
+    );
+  }
 }
+
 
 export default Bio;
 
+
 Bio.propTypes = {
+  name: PropTypes.string.isRequired,
   bio: PropTypes.string,
   title: PropTypes.string,
   visibility: PropTypes.oneOf(['Everyone', 'Just me']),
+  mode: PropTypes.oneOf(['static', 'editable', 'editing']),
+  onEdit: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
 };
 
 Bio.defaultProps = {
   bio: '',
   title: 'About',
   visibility: 'Everyone',
+  mode: 'editable',
 };
---
+++ src/components/UserAccount/TransitionReplace.jsx
@@ -0,0 +1,148 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Transition, TransitionGroup } from 'react-transition-group';
+
+class TransitionReplace extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: null,
+    };
+
+    this.onChildExit = this.onChildExit.bind(this);
+    this.onChildEntering = this.onChildEntering.bind(this);
+    this.onChildrenExited = this.onChildrenExited.bind(this);
+  }
+
+  // Note: Transition events are fired in this order:
+  // onEnter > onEntering > onEntered
+  // onExit  > onExiting  > onExited
+  //
+  // Keep in mind that we always have two transitions happening
+  // The height animation relies on measurements from both the exiting
+  // node and the entering node at key moments.
+
+  onChildExit(htmlNode) {
+    // subtract 1 from the height to improve shifts at the end of the animation
+    this.setState({
+      height: htmlNode.offsetHeight - 1,
+    });
+  }
+
+  onChildEntering(htmlNode) {
+    // subtract 1 from the height to improve shifts at the end of the animation
+    this.setState({
+      height: htmlNode.offsetHeight - 1,
+    });
+  }
+
+  onChildrenExited() {
+    this.setState({
+      height: null,
+    });
+  }
+
+  render() {
+    const {
+      duration,
+      enterFadeEaseFunction,
+      exitFadeEaseFunction,
+      heightChangeEaseFunction,
+    } = this.props;
+
+
+    // Styles for the Transition
+
+    const defaultStyle = {
+      opacity: 0,
+      padding: '1px 0',
+      margin: '-1px 0',
+    };
+
+    const transitionStyles = {
+      entering: {
+        opacity: 1,
+        position: 'relative',
+        zIndex: 1,
+        transition: `opacity ${duration}ms ${enterFadeEaseFunction}`,
+      },
+      entered: {
+        opacity: 1,
+        position: 'relative',
+        zIndex: 1,
+      },
+      exiting: {
+        opacity: 0,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        pointerEvents: 'none',
+        transition: `opacity ${duration}ms ${exitFadeEaseFunction}`,
+      },
+      exited: {
+        opacity: 0,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+      },
+    };
+
+    const transitionProps = {
+      timeout: duration,
+      unmountOnExit: true,
+      mountOnEnter: true,
+      onExit: this.onChildExit,
+      onEntering: this.onChildEntering,
+      onExited: this.onChildrenExited,
+    };
+
+    return (
+      <div
+        style={{
+          position: 'relative',
+          overflow: this.state.height === null ? 'auto' : 'hidden',
+          height: this.state.height, // prevent rounding shifts from being too noticeable
+          transition: `height ${duration}ms ${heightChangeEaseFunction}`,
+        }}
+      >
+        <TransitionGroup>
+          {React.Children.map(this.props.children, child => (
+            <Transition {...transitionProps}>
+              {state => (
+                <div
+                  style={{
+                    ...defaultStyle,
+                    ...transitionStyles[state],
+                  }}
+                >
+                  {child}
+                </div>
+              )}
+            </Transition>
+          ))}
+        </TransitionGroup>
+      </div>
+    );
+  }
+}
+
+export default TransitionReplace;
+
+
+TransitionReplace.propTypes = {
+  children: PropTypes.element.isRequired,
+  duration: PropTypes.number,
+  enterFadeEaseFunction: PropTypes.string,
+  exitFadeEaseFunction: PropTypes.string,
+  heightChangeEaseFunction: PropTypes.string,
+};
+
+TransitionReplace.defaultProps = {
+  duration: 300,
+  enterFadeEaseFunction: 'ease',
+  exitFadeEaseFunction: 'ease',
+  heightChangeEaseFunction: 'ease-in-out',
+};
