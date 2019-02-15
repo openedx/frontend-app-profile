304fc4c08a4a45e51db071ea3d033dd93ad53626
Adam Butterworth <abutterworth@edx.org>
2019-02-15 15:53:50 -0500

    Add action creator tests

--- src/actions/profile.test.js
+++ src/actions/profile.test.js
@@ -5,6 +5,22 @@ import {
   saveUserProfileFailure,
   saveUserProfileReset,
   saveUserProfile,
+  SAVE_USER_PROFILE_PHOTO,
+  saveUserProfilePhotoBegin,
+  saveUserProfilePhotoSuccess,
+  saveUserProfilePhotoFailure,
+  saveUserProfilePhotoReset,
+  saveUserProfilePhoto,
+  DELETE_USER_PROFILE_PHOTO,
+  deleteUserProfilePhotoBegin,
+  deleteUserProfilePhotoSuccess,
+  deleteUserProfilePhotoFailure,
+  deleteUserProfilePhotoReset,
+  deleteUserProfilePhoto,
+  EDITABLE_FIELD_OPEN,
+  EDITABLE_FIELD_CLOSE,
+  openEditableField,
+  closeEditableField,
 } from './profile';
 
 describe('SAVE profile actions', () => {
@@ -57,5 +73,114 @@ describe('SAVE profile actions', () => {
       payload: { error },
     };
     expect(saveUserProfileFailure(error)).toEqual(expectedAction);
+  });
+});
+
+
+describe('SAVE profile photo actions', () => {
+  it('should create an action to signal the start of a profile photo save', () => {
+    const formData = 'multipart form data';
+    const expectedAction = {
+      type: SAVE_USER_PROFILE_PHOTO.BASE,
+      payload: {
+        username: 'myusername',
+        formData,
+      },
+    };
+    expect(saveUserProfilePhoto('myusername', formData)).toEqual(expectedAction);
+  });
+
+  it('should create an action to signal user profile photo save beginning', () => {
+    const expectedAction = {
+      type: SAVE_USER_PROFILE_PHOTO.BEGIN,
+    };
+    expect(saveUserProfilePhotoBegin()).toEqual(expectedAction);
+  });
+
+  it('should create an action to signal user profile photo save success', () => {
+    const expectedAction = {
+      type: SAVE_USER_PROFILE_PHOTO.SUCCESS,
+    };
+    expect(saveUserProfilePhotoSuccess()).toEqual(expectedAction);
+  });
+
+  it('should create an action to signal user profile photo save success', () => {
+    const expectedAction = {
+      type: SAVE_USER_PROFILE_PHOTO.RESET,
+    };
+    expect(saveUserProfilePhotoReset()).toEqual(expectedAction);
+  });
+
+  it('should create an action to signal user profile photo save failure', () => {
+    const error = 'Test failure';
+    const expectedAction = {
+      type: SAVE_USER_PROFILE_PHOTO.FAILURE,
+      payload: { error },
+    };
+    expect(saveUserProfilePhotoFailure(error)).toEqual(expectedAction);
+  });
+});
+
+
+describe('DELETE profile photo actions', () => {
+  it('should create an action to signal the start of a profile photo deletion', () => {
+    const expectedAction = {
+      type: DELETE_USER_PROFILE_PHOTO.BASE,
+      payload: {
+        username: 'myusername',
+      },
+    };
+    expect(deleteUserProfilePhoto('myusername')).toEqual(expectedAction);
+  });
+
+  it('should create an action to signal user profile photo deletion beginning', () => {
+    const expectedAction = {
+      type: DELETE_USER_PROFILE_PHOTO.BEGIN,
+    };
+    expect(deleteUserProfilePhotoBegin()).toEqual(expectedAction);
+  });
+
+  it('should create an action to signal user profile photo deletion success', () => {
+    const expectedAction = {
+      type: DELETE_USER_PROFILE_PHOTO.SUCCESS,
+    };
+    expect(deleteUserProfilePhotoSuccess()).toEqual(expectedAction);
+  });
+
+  it('should create an action to signal user profile photo deletion success', () => {
+    const expectedAction = {
+      type: DELETE_USER_PROFILE_PHOTO.RESET,
+    };
+    expect(deleteUserProfilePhotoReset()).toEqual(expectedAction);
+  });
+
+  it('should create an action to signal user profile photo deletion failure', () => {
+    const error = 'Test failure';
+    const expectedAction = {
+      type: DELETE_USER_PROFILE_PHOTO.FAILURE,
+      payload: { error },
+    };
+    expect(deleteUserProfilePhotoFailure(error)).toEqual(expectedAction);
+  });
+});
+
+
+describe('Editable field opening and closing actions', () => {
+  const fieldName = 'fullName';
+
+  it('should create an action to signal the opening a field', () => {
+    const expectedAction = {
+      type: EDITABLE_FIELD_OPEN,
+      fieldName,
+    };
+    expect(openEditableField(fieldName)).toEqual(expectedAction);
+  });
+
+  it('should create an action to signal the closing a field', () => {
+    const expectedAction = {
+      type: EDITABLE_FIELD_CLOSE,
+      fieldName,
+    };
+    expect(closeEditableField(fieldName)).toEqual(expectedAction);
   });
 });
