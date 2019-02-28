da7da03a1904d483d275119836acc1c8eb7bf1f1
Adam Butterworth <abutterworth@edx.org>
2019-02-28 15:16:33 -0500

    Add MicroMasters illustration in background of all certificates

--- src/components/ProfilePage/Certificates.messages.jsx
+++ src/components/ProfilePage/Certificates.messages.jsx
@@ -6,6 +6,11 @@ const messages = defineMessages({
     defaultMessage: 'My Certificates',
     description: 'A section of a user profile',
   },
+  'profile.certificates.view.certificate': {
+    id: 'profile.certificates.view.certificate',
+    defaultMessage: 'View Certificate',
+    description: 'A call to action to view a certificate',
+  },
 });
 
 export default messages;
