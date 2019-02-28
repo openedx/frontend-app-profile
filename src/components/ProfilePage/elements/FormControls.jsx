663d9964e93901250a499872bbefc9f6f589ce28
Adam Butterworth <abutterworth@edx.org>
2019-02-28 16:39:44 -0500

    Flip buttons and put visibility on new line

--- src/components/ProfilePage/elements/FormControls.jsx
+++ src/components/ProfilePage/elements/FormControls.jsx
@@ -1,45 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Input, Button, Label, Row, Col } from 'reactstrap';
+import { Button, Label, FormGroup } from 'reactstrap';
 import { injectIntl, intlShape } from 'react-intl';
 
 import messages from './FormControls.messages';
 
 import AsyncActionButton from './AsyncActionButton';
+import VisibilitySelect from './VisibilitySelect';
 
 function FormControls({
   formId, cancelHandler, changeHandler, visibility, saveState, intl,
 }) {
   const visibilityId = `${formId}-visibility`;
   return (
-    <Row className="align-items-center flex-wrap-1 pt-3">
-      <Col xs="auto" className="d-flex mb-3">
-        <Label className="flex-shrink-0 d-inline-block mb-0 mr-2" size="sm" for={visibilityId}>
+    <React.Fragment>
+      <FormGroup className="mb-4">
+        <Label className="mb-1" size="sm" for={visibilityId}>
           {intl.formatMessage(messages['profile.formcontrols.who.can.see'])}
         </Label>
-        <span>
-          <Input
-            id={visibilityId}
-            className="d-inline-block"
-            bsSize="sm"
-            type="select"
-            name="visibility"
-            value={visibility}
-            onChange={changeHandler}
-          >
-            <option key="private" value="private">
-              {intl.formatMessage(messages['profile.formcontrols.who.just.me'])}
-            </option>
-            <option key="all_users" value="all_users">
-              {intl.formatMessage(messages['profile.formcontrols.who.everyone'])}
-            </option>
-          </Input>
-        </span>
-      </Col>
-      <Col xs="auto" className="flex-grow-1 d-flex justify-content-end mb-3">
-        <Button color="link" onClick={cancelHandler}>
-          {intl.formatMessage(messages['profile.formcontrols.button.cancel'])}
-        </Button>
+        <VisibilitySelect
+          id={visibilityId}
+          className="w-auto"
+          type="select"
+          name="visibility"
+          value={visibility}
+          onChange={changeHandler}
+        />
+      </FormGroup>
+      <FormGroup>
         <AsyncActionButton
           type="submit"
           variant={saveState}
@@ -50,8 +38,11 @@ function FormControls({
             error: intl.formatMessage(messages['profile.formcontrols.button.save.failed']),
           }}
         />
-      </Col>
-    </Row>
+        <Button color="link" onClick={cancelHandler}>
+          {intl.formatMessage(messages['profile.formcontrols.button.cancel'])}
+        </Button>
+      </FormGroup>
+    </React.Fragment>
   );
 }
 
