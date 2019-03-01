8063c0453d499e5fb863603dd88c74c19060f823
Adam Butterworth <abutterworth@edx.org>
2019-03-01 11:51:09 -0500

    Clean up message ids

--- src/components/ProfilePage/elements/VisibilitySelect.messages.jsx
+++ src/components/ProfilePage/elements/VisibilitySelect.messages.jsx
@@ -1,45 +1,15 @@
 import { defineMessages } from 'react-intl';
 
 const messages = defineMessages({
-  'profile.formcontrols.who.can.see': {
-    id: 'profile.formcontrols.who.can.see',
-    defaultMessage: 'Who can see this:',
-    description: 'What users can see this area?',
-  },
-  'profile.formcontrols.who.just.me': {
-    id: 'profile.formcontrols.who.just.me',
+  'profile.visibilityselect.who.just.me': {
+    id: 'profile.visibilityselect.who.just.me',
     defaultMessage: 'Just me',
     description: 'What users can see this area?',
   },
-  'profile.formcontrols.who.everyone': {
-    id: 'profile.formcontrols.who.everyone',
+  'profile.visibilityselect.who.everyone': {
+    id: 'profile.visibilityselect.who.everyone',
     defaultMessage: 'Everyone on edX',
     description: 'What users can see this area?',
-  },
-  'profile.formcontrols.button.cancel': {
-    id: 'profile.formcontrols.button.cancel',
-    defaultMessage: 'Cancel',
-    description: 'A button label',
-  },
-  'profile.formcontrols.button.save': {
-    id: 'profile.formcontrols.button.save',
-    defaultMessage: 'Save',
-    description: 'A button label',
-  },
-  'profile.formcontrols.button.saving': {
-    id: 'profile.formcontrols.button.saving',
-    defaultMessage: 'Saving',
-    description: 'A button label',
-  },
-  'profile.formcontrols.button.saved': {
-    id: 'profile.formcontrols.button.saved',
-    defaultMessage: 'Saved',
-    description: 'A button label',
-  },
-  'profile.formcontrols.button.save.failed': {
-    id: 'profile.formcontrols.button.save.failed',
-    defaultMessage: 'Save Failed',
-    description: 'A button label',
   },
 });
 
