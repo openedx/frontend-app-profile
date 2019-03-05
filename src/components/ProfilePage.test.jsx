11252f30072013048d302e70f8478e268946e006
Robert Raposa <rraposa@edx.org>
2019-03-05 01:37:00 -0500

    Temporary rename to edx.bi.profile.viewed.

--- src/components/ProfilePage.test.jsx
+++ src/components/ProfilePage.test.jsx
@@ -39,9 +39,9 @@ describe('<ProfilePage />', () => {
       ));
 
       expect(analytics.logEvent.mock.calls.length).toBe(1);
-      expect(analytics.logEvent.mock.calls[0][0]).toEqual('edx.profile.viewed');
+      expect(analytics.logEvent.mock.calls[0][0]).toEqual('edx.bi.profile.viewed');
       expect(analytics.logEvent.mock.calls[0][1]).toEqual({
-        profileUsername: 'test-username',
+        username: 'test-username',
       });
     });
   });
