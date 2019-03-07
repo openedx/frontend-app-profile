07a3680d5e4751ab06c9f305fa31c96ea5433c47
Adam Butterworth <adamjbutterworth@gmail.com>
2019-03-07 15:23:55 -0500

    feat(layout): add layout and style to 404 page (#63)

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
 import { FormattedMessage } from 'react-intl';
 
 export default class NotFoundPage extends Component {
@@ -6,13 +7,21 @@ export default class NotFoundPage extends Component {
 
   render() {
     return (
-      <div>
-        <FormattedMessage
-          id="profile.notfound.message"
-          defaultMessage="The page you're looking for is unavailable or there's an error in the URL. Please check the URL and try again."
-          description="error message when a page does not exist"
-        />
-      </div>
+      <Container
+        fluid
+        className="d-flex py-5 justify-content-center align-items-start text-center"
+      >
+        <p
+          className="my-0 py-5 text-muted"
+          style={{ maxWidth: '32em' }}
+        >
+          <FormattedMessage
+            id="profile.notfound.message"
+            defaultMessage="The page you're looking for is unavailable or there's an error in the URL. Please check the URL and try again."
+            description="error message when a page does not exist"
+          />
+        </p>
+      </Container>
     );
   }
 }
