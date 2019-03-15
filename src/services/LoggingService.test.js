5d9c9b52cfc332b17ee041c43f6a75f58e9526e8
Robert Raposa <rraposa@edx.org>
2019-03-15 17:52:31 -0400

    Enhance the logging service.
    - Add custom attributes to api response errors.
    - Add custom attribute capabilities to loadError.
    - Drop the data for html page responses.
    - Move code for max message length.
    - Add tests.
    
    ARCH-578

--- src/services/LoggingService.test.js
+++ src/services/LoggingService.test.js
@@ -1,4 +1,4 @@
-import LoggingService from './LoggingService';
+import LoggingService, { MAX_ERROR_LENGTH } from './LoggingService';
 
 global.newrelic = {
   addPageAction: jest.fn(),
@@ -6,6 +6,10 @@ global.newrelic = {
 };
 
 describe('logInfo', () => {
+  beforeEach(() => {
+    global.newrelic.addPageAction.mockReset();
+  });
+
   it('calls New Relic client to log message if the client is available', () => {
     const message = 'Test log';
     LoggingService.logInfo(message);
@@ -14,15 +18,43 @@ describe('logInfo', () => {
 });
 
 describe('logError', () => {
+  beforeEach(() => {
+    global.newrelic.noticeError.mockReset();
+  });
+
   it('calls New Relic client to log error if the client is available', () => {
     const error = new Error('Failed!');
     LoggingService.logError(error);
-    expect(global.newrelic.noticeError).toHaveBeenCalledWith(error);
+    expect(global.newrelic.noticeError).toHaveBeenCalledWith(error, undefined);
+  });
+
+  it('calls New Relic client to log error if the client is available', () => {
+    const error = new Error('Failed!');
+    LoggingService.logError(error);
+    expect(global.newrelic.noticeError).toHaveBeenCalledWith(error, undefined);
+  });
+
+  it('calls New Relic client with truncated error string', () => {
+    const error = new Array(MAX_ERROR_LENGTH + 500 + 1).join('0');
+    const expectedError = new Array(MAX_ERROR_LENGTH + 1).join('0');
+    LoggingService.logError(error);
+    expect(global.newrelic.noticeError).toHaveBeenCalledWith(expectedError, undefined);
+  });
+
+  it('calls New Relic client with truncated error', () => {
+    const error = {
+      message: new Array(MAX_ERROR_LENGTH + 500 + 1).join('0'),
+    };
+    const expectedError = {
+      message: new Array(MAX_ERROR_LENGTH + 1).join('0'),
+    };
+    LoggingService.logError(error);
+    expect(global.newrelic.noticeError).toHaveBeenCalledWith(expectedError, undefined);
   });
 });
 
 describe('logAPIErrorResponse', () => {
-  afterEach(() => {
+  beforeEach(() => {
     global.newrelic.noticeError.mockReset();
   });
 
@@ -36,14 +68,20 @@ describe('logAPIErrorResponse', () => {
     };
     const message = `${error.request.status} ${error.request.responseURL} ${error.request.responseText}`;
     const expectedError = new Error(`API request failed: ${message}`);
+    const expectedAttributes = {
+      errorType: 'api-request-error',
+      errorStatus: error.request.status,
+      errorUrl: error.request.responseURL,
+      errorData: error.request.responseText,
+    };
     LoggingService.logAPIErrorResponse(error);
-    expect(global.newrelic.noticeError).toHaveBeenCalledWith(expectedError);
+    expect(global.newrelic.noticeError).toHaveBeenCalledWith(expectedError, expectedAttributes);
   });
 
   it('calls New Relic client to log error when error has response object', () => {
     const error = {
       response: {
-        status: 400,
+        status: 500,
         config: {
           url: 'http://example.com',
         },
@@ -54,7 +92,33 @@ describe('logAPIErrorResponse', () => {
     };
     const message = `${error.response.status} ${error.response.config.url} ${JSON.stringify(error.response.data)}`;
     const expectedError = new Error(`API request failed: ${message}`);
+    const expectedAttributes = {
+      errorType: 'api-response-error',
+      errorStatus: error.response.status,
+      errorUrl: error.response.config.url,
+      errorData: JSON.stringify(error.response.data),
+      test: 'custom',
+    };
+    LoggingService.logAPIErrorResponse(error, { test: 'custom' });
+    expect(global.newrelic.noticeError).toHaveBeenCalledWith(expectedError, expectedAttributes);
+  });
+
+  it('calls New Relic client to log error and ignores html content in response data.', () => {
+    const error = {
+      response: {
+        status: 500,
+        data: '<!DOCTYPE html><html lang="en"><body></body></html>',
+      },
+    };
+    const message = `${error.response.status}  `;
+    const expectedError = new Error(`API request failed: ${message}`);
+    const expectedAttributes = {
+      errorType: 'api-response-error',
+      errorStatus: error.response.status,
+      errorUrl: '',
+      errorData: '',
+    };
     LoggingService.logAPIErrorResponse(error);
-    expect(global.newrelic.noticeError).toHaveBeenCalledWith(expectedError);
+    expect(global.newrelic.noticeError).toHaveBeenCalledWith(expectedError, expectedAttributes);
   });
 });
