9e2ee48ab566bf9064d2a0a38723269aaf5b333c
Diana Catalina Olarte <diana.olarte@edunext.co>
2022-10-11 14:27:59 -0500

    feat: allow runtime configuration

---
+++ src/head/messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  'profile.page.title': {
+    id: 'profile.page.title',
+    defaultMessage: 'Profile | {siteName}',
+    description: 'Title tag',
+  },
+});
+
+export default messages;
