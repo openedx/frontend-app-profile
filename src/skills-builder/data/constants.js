8f42e6fbfbed8a4c3dc5c2cefe4a2f588ba11c82
Maxwell Frank <mfrank@2u.com>
2023-02-01 21:46:44 +0000

    feat: adding actions and reducer

--- src/skills-builder/data/constants.js
+++ src/skills-builder/data/constants.js
@@ -1,2 +1,7 @@
-// eslint-disable-next-line import/prefer-default-export
 export const SET_GOAL = 'SET_GOAL';
+export const SET_CURRENT_JOB_TITLE = 'SET_CURRENT_JOB_TITLE';
+export const ADD_CAREER_INTEREST = 'ADD_CAREER_INTEREST';
+export const REMOVE_CAREER_INTEREEST = 'REMOVE_CAREER_INTEREEST';
+
+export const STEP1 = 'select-your-preferences';
+export const STEP2 = 'review-your-results';
---
+++ src/skills-builder/skills-builder-modal/ViewResults.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ViewResults = () => (
+  <h3>Results will render on this step</h3>
+);
+
+export default ViewResults;
