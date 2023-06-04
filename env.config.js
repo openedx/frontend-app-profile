9a7a58b53951601814456febce7c520c051acd82
Adam Stankiewicz <agstanki@gmail.com>
2023-06-04 17:12:20 -0400

    fix: use brandOverrides

--- env.config.js
+++ env.config.js
@@ -1,9 +1,17 @@
 const config = {
   PARAGON_THEME_URLS: {
-    core: 'https://cdn.jsdelivr.net/npm/@edx/paragon@$paragonVersion/dist/core.min.css',
+    core: {
+      urls: {
+        default: 'https://cdn.jsdelivr.net/npm/@edx/paragon@$paragonVersion/dist/core.min.css',
+        brandOverride: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@2.2.0-alpha.9/dist/core.min.css',
+      },
+    },
     variants: {
       light: {
-        url: 'https://cdn.jsdelivr.net/npm/@edx/paragon@$paragonVersion/dist/light.min.css',
+        urls: {
+          default: 'https://cdn.jsdelivr.net/npm/@edx/paragon@$paragonVersion/dist/light.min.css',
+          brandOverride: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@2.2.0-alpha.9/dist/light.min.css',
+        },
         default: true,
         dark: false,
       },
