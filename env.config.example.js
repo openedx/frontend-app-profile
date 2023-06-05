c5d65ccf5c6f9588f0ee6fd83a418d611ad0fd55
Adam Stankiewicz <agstanki@gmail.com>
2023-06-05 01:04:56 -0400

    fix: use brandVersion in PARAGON_THEME_URLs

--- env.config.example.js
+++ env.config.example.js
@@ -3,14 +3,14 @@ const config = {
     core: {
       urls: {
         default: 'https://cdn.jsdelivr.net/npm/@edx/paragon@$paragonVersion/dist/core.min.css',
-        brandOverride: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@2.2.0-alpha.9/dist/core.min.css',
+        brandOverride: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@$brandVersion/dist/core.min.css',
       },
     },
     variants: {
       light: {
         urls: {
           default: 'https://cdn.jsdelivr.net/npm/@edx/paragon@$paragonVersion/dist/light.min.css',
-          brandOverride: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@2.2.0-alpha.9/dist/light.min.css',
+          brandOverride: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@$brandVersion/dist/light.min.css',
         },
         default: true,
         dark: false,
