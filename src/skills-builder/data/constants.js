5e0eef68e306566ed6e8d34beec2f2ef5c0fe673
Maxwell Frank <mfrank@2u.com>
2023-06-28 18:16:10 +0000

    refactor: remove Skills Builder

--- src/skills-builder/data/constants.js
+++
@@ -1,9 +0,0 @@
-// Actions for Skills Context
-export const SET_GOAL = 'SET_GOAL';
-export const SET_CURRENT_JOB_TITLE = 'SET_CURRENT_JOB_TITLE';
-export const ADD_CAREER_INTEREST = 'ADD_CAREER_INTEREST';
-export const REMOVE_CAREER_INTEREST = 'REMOVE_CAREER_INTEREST';
-
-// Stepper keys
-export const STEP1 = 'select-your-preferences';
-export const STEP2 = 'review-your-results';
