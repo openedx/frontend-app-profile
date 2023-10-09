d837e0372e608850f6dd6710cd81f468369ffddb
vladislavkeblysh <vladislav.keblysh@raccoongang.com>
2023-10-09 19:13:22 +0300

    feat: fixed conflicts

--- src/profile/ProfilePage.jsx
+++ src/profile/ProfilePage.jsx
@@ -33,6 +33,7 @@ import DateJoined from './DateJoined';
 import UsernameDescription from './UsernameDescription';
 import PageLoading from './PageLoading';
 import Banner from './Banner';
+import LearningGoal from './forms/LearningGoal';
 
 // Selectors
 import { profilePageSelector } from './data/selectors';
@@ -184,6 +185,8 @@ class ProfilePage extends React.Component {
       socialLinks,
       draftSocialLinksByPlatform,
       visibilitySocialLinks,
+      learningGoal,
+      visibilityLearningGoal,
       languageProficiencies,
       visibilityLanguageProficiencies,
       courseCertificates,
@@ -299,6 +302,14 @@ class ProfilePage extends React.Component {
                 bio={bio}
                 visibilityBio={visibilityBio}
                 formId="bio"
+                {...commonFormProps}
+              />
+            )}
+            {getConfig().ENABLE_SKILLS_BUILDER_PROFILE && (
+              <LearningGoal
+                learningGoal={learningGoal}
+                visibilityLearningGoal={visibilityLearningGoal}
+                formId="learningGoal"
                 {...commonFormProps}
               />
             )}
