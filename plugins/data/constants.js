9291ea654a5ae027928ec2beffb7285f90bfdb46
Jason Wesson <jsnwesson@gmail.com>
2023-10-13 10:50:57 -0700

    build: Profile Plugin Page (#868)
    * build: create Profile Plugin Page
    
    * add Plugin Framework to top of Profile directory
    
    * build: wrap Profile Plugin Page with Plugin
    
    ---------
    
    Co-authored-by: Jason Wesson <jwesson@2u.com>

---
+++ plugins/data/constants.js
@@ -0,0 +1,8 @@
+// TODO: We expect other plugin types to be added here, such as LTI_PLUGIN and BUILD_TIME_PLUGIN.
+export const IFRAME_PLUGIN = 'IFRAME_PLUGIN'; // loads iframe at the URL, rather than loading a JS file.
+
+// Plugin lifecycle events
+export const PLUGIN_MOUNTED = 'PLUGIN_MOUNTED';
+export const PLUGIN_READY = 'PLUGIN_READY';
+export const PLUGIN_UNMOUNTED = 'PLUGIN_UNMOUNTED';
+export const PLUGIN_RESIZE = 'PLUGIN_RESIZE';
