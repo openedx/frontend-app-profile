06e827bdb4763fa6d75eba15f907372d90ca0b00
vladislavkeblysh <138868841+vladislavkeblysh@users.noreply.github.com>
2023-10-16 20:06:59 +0300

    feat: Fixed displaying banner and certificates on mobile view (palm.master) (#861)
    * feat: fixed displaing banner and certificates on mobile view
    
    * feat: update snapshot

--- src/profile/Banner.jsx
+++ src/profile/Banner.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Banner = () => <div className="profile-page-bg-banner bg-primary d-none d-md-block p-relative" />;
+const Banner = () => <div className="profile-page-bg-banner bg-primary d-md-block p-relative" />;
 
 export default Banner;
