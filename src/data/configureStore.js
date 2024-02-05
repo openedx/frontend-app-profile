a87e2bb4958584ec0d9d1ac149c83ecb8b22d1b0
Maxwell Frank <mfrank@2u.com>
2024-02-05 18:23:59 +0000

    fix: dependency import

--- src/data/configureStore.js
+++ src/data/configureStore.js
@@ -1,7 +1,7 @@
 import { getConfig } from '@edx/frontend-platform';
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
