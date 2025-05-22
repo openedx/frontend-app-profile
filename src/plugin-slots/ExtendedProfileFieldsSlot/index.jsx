9eaa1a22fe9631eaa18c71d901f8fa7335b74010
Brayan Ceron <brayan.ceron@edunext.co>
2025-05-22 14:27:40 -0500

    feat: refactor extended profile fields implementation and remove unused components

--- src/plugin-slots/ExtendedProfileFieldsSlot/index.jsx
+++ src/plugin-slots/ExtendedProfileFieldsSlot/index.jsx
@@ -1,13 +1,31 @@
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
-import ExtendedProfileFields from '../../profile/extended-profile';
+import { useDispatch, useSelector } from 'react-redux';
 
-const ExtendedProfileFieldsSlot = () => (
-  <PluginSlot
-    id="org.openedx.frontend.profile.extended_profile_fields.v1"
-    idAliases={['extended_profile_fields_plugin']}
-  >
-    <ExtendedProfileFields />
-  </PluginSlot>
-);
+import { patchProfile } from '../../profile/data/services';
+import { fetchProfile } from '../../profile/data/actions';
+
+import SwitchContent from '../../profile/forms/elements/SwitchContent';
+import EmptyContent from '../../profile/forms/elements/EmptyContent';
+import EditableItemHeader from '../../profile/forms/elements/EditableItemHeader';
+
+const ExtendedProfileFieldsSlot = () => {
+  const dispatch = useDispatch();
+  const extendedProfileValues = useSelector((state) => state.profilePage.account.extendedProfile);
+
+  return (
+    <PluginSlot
+      id="org.openedx.frontend.profile.extended_profile_fields.v1"
+      idAliases={['extended_profile_fields_slot']}
+      pluginProps={{
+        fetchProfile: (username) => dispatch(fetchProfile(username)),
+        patchProfile,
+        extendedProfileValues,
+        SwitchContent,
+        EmptyContent,
+        EditableItemHeader,
+      }}
+    />
+  );
+};
 
 export default ExtendedProfileFieldsSlot;
--- src/profile/extended-profile/constants.js
+++
@@ -1,6 +0,0 @@
-export const FORM_MODE = {
-  EDITING: 'editing',
-  EDITABLE: 'editable',
-  EMPTY: 'empty',
-  STATIC: 'static',
-};
