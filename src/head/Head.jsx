98759d5769a41d292089ae6fd008d9a80db9480d
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-09-25 11:39:01 -0600

    chore: removing react helmet

--- src/head/Head.jsx
+++ src/head/Head.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import { getConfig } from '@edx/frontend-platform';
 
 import messages from './messages';
 
 const Head = ({ intl }) => (
-  <Helmet>
+  <>
     <title>
       {intl.formatMessage(messages['profile.page.title'], { siteName: getConfig().SITE_NAME })}
     </title>
     <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
-  </Helmet>
+  </>
 );
 
 Head.propTypes = {
