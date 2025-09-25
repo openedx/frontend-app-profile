dbadbe8892a3af960fabe3265913f239f0638545
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-09-25 11:39:01 -0600

    chore: removing react helmet

--- src/head/Head.test.jsx
+++ src/head/Head.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
-import { Helmet } from 'react-helmet';
 import { render } from '@testing-library/react';
 import { getConfig } from '@edx/frontend-platform';
 import Head from './Head';
@@ -9,9 +8,7 @@ describe('Head', () => {
   const props = {};
   it('should match render title tag and favicon with the site configuration values', () => {
     render(<IntlProvider locale="en"><Head {...props} /></IntlProvider>);
-    const helmet = Helmet.peek();
-    expect(helmet.title).toEqual(`Profile | ${getConfig().SITE_NAME}`);
-    expect(helmet.linkTags[0].rel).toEqual('shortcut icon');
-    expect(helmet.linkTags[0].href).toEqual(getConfig().FAVICON_URL);
+    expect(document.title).toEqual(`Profile | ${getConfig().SITE_NAME}`);
+    expect(document.querySelector('link[rel="shortcut icon"]').href).toEqual(getConfig().FAVICON_URL);
   });
 });
