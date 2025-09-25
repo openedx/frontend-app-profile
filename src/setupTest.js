884b7d206751f69fae98bb43579df0d505120124
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-09-25 15:11:05 -0600

    chore: update ts, config and ignore files

--- src/setupTest.js
+++ src/setupTest.js
@@ -2,3 +2,8 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom';
+import siteConfig from 'site.config';
+import { mergeSiteConfig } from '@openedx/frontend-base';
+
+mergeSiteConfig(siteConfig);
+addAppConfigs();
