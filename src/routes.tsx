640f941585095d85a412c456fd7ed3c1d9fb69b7
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-09-30 10:53:15 -0600

    chore: fixing routes issue

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,19 @@
 import { ProfilePage, NotFoundPage } from './profile';
 import Main from './Main';
 import { withNavigate } from './utils/hoc';
+import { RoleRouteObject } from '@openedx/frontend-base/types';
 
-const routes = [
+const ProfilePageWithNavigate = withNavigate(ProfilePage);
+
+const routes: RoleRouteObject[] = [
   {
     id: 'org.openedx.frontend.route.profile.main',
+    path: '/',
     Component: Main,
     children: [
       {
         path: '/u/:username',
-        element: (withNavigate(ProfilePage))
+        element: <ProfilePageWithNavigate />
       },
       {
         path: '/notfound',
