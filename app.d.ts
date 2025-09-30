23a1eea65fcc1686e75bc3c2f9926e056c914a0b
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-09-30 11:56:48 -0600

    chore: adding scss ts declaration

--- app.d.ts
+++ app.d.ts
@@ -8,3 +8,18 @@ declare module '*.svg' {
   const content: string;
   export default content;
 }
+
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: Record<string, string>;
+  export default content;
+}
