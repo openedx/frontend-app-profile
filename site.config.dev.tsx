6acaa98d42413d86ce4b03d696962a79452248a2
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-09-30 21:17:37 -0600

    chore: adding courses navlink slot

--- site.config.dev.tsx
+++ site.config.dev.tsx
@@ -31,7 +31,11 @@ const siteConfig: SiteConfig = {
     {
       role: 'org.openedx.frontend.role.logout',
       url: 'http://local.openedx.io:8000/logout'
-    }
+    },
+    {
+      role: 'org.openedx.frontend.role.learnerDashboard',
+      url: 'http://apps.local.openedx.io:1996/learner-dashboard'
+    },
   ],
 };
 
--- src/slots.tsx
+++ src/slots.tsx
@@ -1,6 +1,15 @@
-import { SlotOperation } from '@openedx/frontend-base';
+import { SlotOperation, WidgetOperationTypes } from '@openedx/frontend-base';
+import CoursesMenuItem from './widgets/CoursesMenuItem';
 
 const slots: SlotOperation[] = [
+  {
+    slotId: 'org.openedx.frontend.slot.header.primaryLinks.v1',
+    id: 'org.openedx.frontend.widget.coursesMenu.headerLink',
+    op: WidgetOperationTypes.APPEND,
+    element: (
+      <CoursesMenuItem label="Courses" role="org.openedx.frontend.role.learnerDashboard" variant="navLink" />
+    )
+  },
 ];
 
 export default slots;
